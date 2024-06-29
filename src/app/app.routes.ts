ef1901827f5857973cd11f0e9b249c62fdac5df4
vova <test@test.test>
2024-06-29 12:10:49 +0300

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
+import { PageExampleComponent } from './components/page-example/page-example.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '',
+		pathMatch: 'full',
+		component: PageExampleComponent
+	}
+];
