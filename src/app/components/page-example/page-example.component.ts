f00bc696efb7ba0b4bc4a08d5b24400804476e3a
vova <test@test.test>
2024-06-29 13:17:55 +0300

    add listen focus input

--- src/app/components/page-example/page-example.component.ts
+++ src/app/components/page-example/page-example.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-page-example',
@@ -7,6 +7,21 @@ import { Component } from '@angular/core';
   templateUrl: './page-example.component.html',
   styleUrl: './page-example.component.scss'
 })
-export class PageExampleComponent {
+export class PageExampleComponent implements OnInit {
+
+  initailHeight: number = 0
+  outputs: Array<string> = []
+
+  onFocus(e: any): void {
+    this.outputs.push(`focus (now: ${window.innerHeight}, initial: ${this.initailHeight})`)
+  }
+
+  onBlur(e: any): void {
+    this.outputs.push(`unfocus (now: ${window.innerHeight}, initial: ${this.initailHeight})`)
+  }
+
+  ngOnInit(): void {
+    this.initailHeight = window.innerHeight
+  }
 
 }
